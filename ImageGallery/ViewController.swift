f6bf4c0395bf84d5b6d1188211004fadfd8410e1
Nik <n.kolesnikov@hotmail.com>
2020-11-13 10:04:04 +0300

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Nik on 13.11.2020.
+//  Copyright © 2020 Nik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
